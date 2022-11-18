8094712864d2df375dab037fd925df8b6c6a9bdd
AlicjaPiaskowska <68071365+AlicjaPiaskowska@users.noreply.github.com>
2022-11-18 15:17:08 +0100

    Update PoC_2_Main.py

--- PoC_2_Main.py
+++ PoC_2_Main.py
@@ -2,12 +2,12 @@ import PoC_2_functions as f
 from flask import Flask
 
 #https://stackoverflow.com/questions/11994325/how-to-divide-flask-app-into-multiple-py-files
-#
+# https://code.visualstudio.com/docs/python/tutorial-flask 
 
 
 URL = "https://www.wroclaw.pl/open-data/87b09b32-f076-4475-8ec9-6020ed1f9ac0/OtwartyWroclaw_rozklad_jazdy_GTFS.zip"
 fileName = "./OtwartyWroclaw_rozklad_jazdy_GTFS.zip"
-path = "./OtwartyWroclaw_rozklad_jazdy_GTFS"
+path_data = "./OtwartyWroclaw_rozklad_jazdy_GTFS"
 txt_file_path = "./OtwartyWroclaw_rozklad_jazdy_GTFS/*.txt"
 
 
@@ -18,17 +18,19 @@ stops_csv = "stops.csv"
 stop_times_csv = "stop_times.csv"
 
 
-
 #Recall of functions
 # downloading data and uploading them to the database
-# f.download_data(URL, fileName, path)
-# f.replace_txt_to_csv(txt_file_path)
+
+#f.delateing_folder_with_data_if_exists(path_data)
+#f.download_data(URL, fileName, path_data)
+#f.replace_txt_to_csv(txt_file_path)
 # f.creating_database(database_name)
-# f.load_write_data(database_name, path, trips_csv, stops_csv, stop_times_csv)
+# f.load_write_data(database_name, path_data, trips_csv, stops_csv, stop_times_csv)
 
 ## Collecting user responses
-# f.index
-# f.user_answers()
+print("Please go to http link")
+f.run_app()
+f.user_answers()
 
 
 ## Calculating the distance and providing a table with the final results
@@ -40,3 +42,4 @@ f.distance_start_point_to_stops()
 f.distance_end_point_to_stops()
 f.distance_start_end_direction()
 f.table_with_routes()
+
